7cd41e32bbfca33df572eec12f9ee279644d3db2
Adalberto Brasaca <aj-brasaca@hotmail.com>
2020-07-22 17:53:18 -0300

    Initializing action as parameter

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -3,6 +3,7 @@ package gui;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.function.Consumer;
 
 import application.Main;
 import gui.util.Alerts;
@@ -33,56 +34,36 @@ public class MainViewController implements Initializable {
 		System.out.println("onMnuItemSellerAction");
 	}
 	
+	// NO MÉTODO loadView FOI UTILIZADO GENERICS PARA NÃO
+	// PRECISAR FAZER DUAS VERSÕES DESSE MÉTODO, JÁ QUE
+	// HÁ A NECESSIDADE DE EXECUTAR UMA AÇÃO NA INICIALIZAÇÃO
+	// E A OUTRA NÃO.
+	
 	@FXML
 	public void onMnuItemDepartmentAction() {
-		loadView2("/gui/DepartmentList.fxml");
+		
+		// O segundo parâmetro é uma ação de inicialização de
+		// DepartmentListController (é uma função lambda passada paro método loadView)
+		loadView("/gui/DepartmentList.fxml", (DepartmentListController controller) -> {
+			controller.setDepartmentService(new DepartmentService());
+			controller.updateTableView();
+		});
 	}
 	
 	@FXML
 	public void onMnuItemAboutAction() {
 		// Carregará o conteúdo desse fxml no fxml da janela principal em runtime.
-		loadView("/gui/About.fxml");
+		// Como aqui não há ação alguma a ser realizada no carregamento da view About,
+		// então o segundo argumento é uma função que não realiza nada.
+		loadView("/gui/About.fxml", x -> {});
 	}
 	
 	@Override
 	public void initialize(URL url, ResourceBundle rb) {
 				
 	}
-	
-	private synchronized void loadView2(String absoluteName) {
 		
-		try {
-			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
-			VBox newVbox = loader.load();
-			
-			// Pegando referência para a cena da janela principal (AULA 272)
-			Scene mainScene = Main.getMainScene();
-			// Pegando referência para a VBox da janela principal (AULA 272)
-			VBox mainVBox = (VBox) ((ScrollPane) mainScene.getRoot()).getContent();
-			// Pegando referência para o menu
-			Node mainMenu = mainVBox.getChildren().get(0);
-			// Limpando todos os filhos do mainVBox
-			mainVBox.getChildren().clear();
-			// Adicionando o menu novamente e os filhos do newVBox
-			mainVBox.getChildren().add(mainMenu);
-			mainVBox.getChildren().addAll(newVbox.getChildren());
-			
-			// REALIZANDO UM PROCESSO MANUAL DE INJETAR A DEPENDÊNCIA NO CONTROLLER
-			// E DEPOIS CHAMAR O MÉTODO PARA ATUALIZAR OS DADOS NA TELA (NO TABLEVIEW)
-			
-			// A partir do objeto "loader" eu posso carregar a View e também acessar o controller
-			// Pegando uma referência para o controler da View que está sendo passada no parâmetro do método
-			DepartmentListController controller = loader.getController();
-			// Injetando a dependência do service lá no controller
-			controller.setDepartmentService(new DepartmentService());
-			controller.updateTableView();
-			
-		} catch (IOException e) {
-			Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
-		}
-}
-	
-	private synchronized void loadView(String absoluteName) {
+	private synchronized <T> void loadView(String absoluteName, Consumer<T> initializingAction) {
 	
 			try {
 				FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
@@ -99,6 +80,16 @@ public class MainViewController implements Initializable {
 				// Adicionando o menu novamente e os filhos do newVBox
 				mainVBox.getChildren().add(mainMenu);
 				mainVBox.getChildren().addAll(newVbox.getChildren());
+				
+				// AS DUAS LINHAS ABAIXO IRÃO EXECUTAR A FUNÇÃO PASSADA COMO ARGUMENTO
+				// NO SEGUNDO PARÂMETRO DA CHAMADA A ESTE MÉTODO
+				
+				// Método getController() irá retornar o tipo do controller
+				// que for passado no segundo parâmetro deste método
+				// O "T" indica o tipo do controler e é genérico então (será do tipo do controller passado)
+				T controller = loader.getController();
+				// Para executar a função passada no segundo parâmetro
+				initializingAction.accept(controller);
 				
 				
 			} catch (IOException e) {
